96271e3483c6e1db2bd8afb90680dd045b72a6e7
Emre KARA <ek@inno.qa>
2026-07-16 15:03:47 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const retryUrl = new URL(window.location.href)
+if (retryUrl.searchParams.delete('__asset_retry')) {
+  window.history.replaceState(window.history.state, '', retryUrl)
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
